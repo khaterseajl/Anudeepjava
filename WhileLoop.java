ba2e699daf073a0ea0046ba5c494d128a8b7fed5
khaterseajl <113005305+khaterseajl@users.noreply.github.com>
2022-09-07 08:52:08 -0400

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,20 @@
+package helloww1;
+  //WAP to print 2 to 5 table using nested while loop?
+public class WhileLoop {
+	    public static void main(String[] args)
+	    {
+	    System.out.println("Tables from 2 to 5 :");
+	           int  i=1;
+	    	      while(i<=4)
+	    	   {	i=i+1;
+	       	      System.out.println("");
+	           int j=0;
+	    		  while(j<=9)
+	    		{    j=j+1;
+	    		  System.out.println(i*j);
+	    			
+	    		}
+	    	}
+	    }
+	    
+}
