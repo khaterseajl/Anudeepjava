2509b32f8407f1b8f5d238a881d7ef35089aede6
khaterseajl <113005305+khaterseajl@users.noreply.github.com>
2022-10-20 07:36:21 -0400

    Update supplythread.java

--- supplythread.java
+++ supplythread.java
@@ -1,17 +1,17 @@
-package Com.producerdemand;
+package Com.producerdemand;    //package name
 
-public class supplythread extends Thread {
+public class supplythread extends Thread {    //supplythread extending thread class
 
-	private Producer pr;
+	private Producer pr;     //data member of producer
 
-	public supplythread(Producer pro) {
+	public supplythread(Producer pro) {   //parameterized constructor
 		super();
-		this.pr = pro;
+		this.pr = pro;   //this keyword represents current instance of
 
 	}
 	
-	public void run()
+	public void run()    //run method get overrided
 	{
-		pr.supply(1000);
+		pr.supply(1000);  //declare supplymethod
 	}
 }
