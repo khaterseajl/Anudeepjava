115b165e8093d4a4b0338d52b43db3909e652687
khaterseajl <113005305+khaterseajl@users.noreply.github.com>
2022-10-20 07:46:26 -0400

    Update Producer.java

--- Producer.java
+++ Producer.java
@@ -5,31 +5,31 @@ package Com.producerdemand;
   Create two threads . In the run() of first thread call demand(int n) and 
   In the run() of second thread call supply(int n).If deamnding number is gretaer than availableProduct
   consumer should wait till supply increases the availableProduct.*/
-public class Producer {
+public class Producer {     //producer class
 
-	private int availableproduct=1000;
+	private int availableproduct=2000;   //declared data members of producer
 	
-	synchronized public void demand(int demandno)
+	synchronized public void demand(int demandno)     //maked demand method synchronized 
 	{
-		System.out.println("available product before demand"+availableproduct);
-		try
+		System.out.println("available product before demand"+availableproduct);  //sop to print on console
+		try    // try block to catch exception
 		{
-			if(demandno>availableproduct)
-			wait();
+			if(demandno>availableproduct)    //if checkes demand number is greater than availableproduct
+			wait();    //method get in waiting state untill demand no is not increased
 			availableproduct-=demandno;
 			System.out.println("available product after demand"+availableproduct);	
 		}
-		catch(InterruptedException ie)
-		{
+		catch(InterruptedException ie)    //catching that  ty block exception 
+		{ 
 			System.out.println(ie);
 		}
 	}
 	
-	synchronized public void supply(int demandno)
+	synchronized public void supply(int demandno)    //maked supply method as a synchronized
 	{
 		System.out.println("available product before supply"+availableproduct);
-		availableproduct+=demandno;
+		availableproduct+=demandno;  //increasing demand no
 		System.out.println("available product after supply"+availableproduct);
-		notify();
+		notify(); // notify which is used to communication between thread
 }
 }
