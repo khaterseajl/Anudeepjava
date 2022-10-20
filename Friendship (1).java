a7e6c4c7d3a1f5132996df101121e401e24d0b75
khaterseajl <113005305+khaterseajl@users.noreply.github.com>
2022-10-20 14:29:48 -0400

    Update Friendship (1).java

--- Friendship (1).java
+++ Friendship (1).java
@@ -1,8 +1,8 @@
 package MutiThread.com;
 class Friendship{
 	synchronized public void classroom(String friendsName ) {
-		String[] strArray = new String[] {"vikas", "Prachi", "sejal", "shri", "omkar", 
-				                          "rutu", "mansi", "amena", "pratik", "shiva"};
+		String[] strArray = new String[] {"Jinesha", "Prachi", "sejal", "shrii", "Shital", 
+				                          "priya", "rutuja", "neha", "payal", "maniii"};
 	        for (String s:strArray) {  
 	        System.out.println(s);  
 		try {
