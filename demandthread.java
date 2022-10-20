0f29c47405f59723631320521c222ee09a83ca64
khaterseajl <113005305+khaterseajl@users.noreply.github.com>
2022-10-20 07:33:25 -0400

    Update demandthread.java

--- demandthread.java
+++ demandthread.java
@@ -1,17 +1,17 @@
-package Com.producerdemand;
+package Com.producerdemand; //package
 
-public class demandthread  extends Thread{
+public class demandthread  extends Thread{ // demandthread extending thread 
 
-	private Producer pro;
+	private Producer pro;   //data member of demandthread
 
-	public demandthread(Producer pro) {
+	public demandthread(Producer pro) {    //parameterized constructor
 		super();
 		this.pro = pro;
 
 	}
 	
-	public void run()
+	public void run()     //run method overrided
 	{
-		pro.demand(1200);
+		pro.demand(1200);   //declare demand method 
 	}
 }
